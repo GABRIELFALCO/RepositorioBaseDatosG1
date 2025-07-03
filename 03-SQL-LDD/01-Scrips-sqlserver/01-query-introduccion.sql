58b03d2ce0112f22bdefd55cbc6db9e4d39f30de
Gabriel Falcon <famg0405@gmail.com>
2025-07-03 15:57:47 -0300

    Logro:construccion de cardinalidades 1 a n, 1 a 1, claves primarias y foraneas compuestas y otras restricciones

--- 03-SQL-LDD/01-Scrips-sqlserver/01-query-introduccion.sql
+++ 03-SQL-LDD/01-Scrips-sqlserver/01-query-introduccion.sql
@@ -75,3 +75,133 @@ VALUES('Burritos de Chorizo de otro 2',100,451.0);
 Go
 
 SELECT * FROM Productos2;
+
+
+
+
+
+
+
+
+
+
+
+--Crear dos tablas con raxon de cardinalidad de 1 a N con particiacion total, esto quiere decir que la FK es NOT NULL
+
+CREATE TABLE categoria(
+categoriaId INT NOT NULL IDENTITY (1,1),
+nombreCategoria NVARCHAR(50) NOT NULL,
+CONSTRAINT pk_categoria
+PRIMARY KEY (categoriaId),
+CONSTRAINT unique_nombrecategoria
+UNIQUE (nombreCategoria)
+);
+Go
+
+CREATE TABLE productos3(
+productoId INT NOT NULL IDENTITY(1,1),
+nombreProducto NVARCHAR(20) NOT NULL,
+existencia INT NOT NULL,
+precioUnitario MONEY NOT NULL,
+categoriaId int not null,
+CONSTRAINT pk_productos3
+PRIMARY KEY(productoId),
+
+CONSTRAINT chk_existencia3
+check(existencia >0 and existencia <=1000),
+CONSTRAINT chk_precioUnitario
+check (precioUnitario>0.0),
+
+CONSTRAINT unique_nombreProducto3
+UNIQUE (nombreProducto),
+--creacion de la fk
+
+CONSTRAINT fk_productos3_categoria
+FOREIGN KEY (categoriaId)
+REFERENCES categoria(categoriaId)
+);
+Go
+
+CREATE TABLE categoria2(
+id INT NOT NULL IDENTITY (1,1),
+nombreCategoria NVARCHAR(50) NOT NULL,
+CONSTRAINT pk_categoria2
+PRIMARY KEY (id),
+CONSTRAINT unique_nombrecategoria2
+UNIQUE (nombreCategoria)
+);
+Go
+
+CREATE TABLE productos4(
+productoId INT NOT NULL IDENTITY(1,1),
+nombreProducto NVARCHAR(20) NOT NULL,
+existencia INT NOT NULL,
+precioUnitario MONEY NOT NULL,
+categoriaId int not null,
+CONSTRAINT pk_productos4
+PRIMARY KEY(productoId),
+CONSTRAINT chk_existencia4
+check(existencia >0 and existencia <=1000),
+CONSTRAINT chk_precioUnitario4
+check (precioUnitario>0.0),
+CONSTRAINT unique_nombreProducto4
+UNIQUE (nombreProducto),
+--creacion de la fk
+CONSTRAINT fk_productos4_categoria
+FOREIGN KEY (categoriaId)
+REFERENCES categoria2(id)
+);
+Go
+
+
+CREATE TABLE tabla1(
+tabla1id INT NOT NULL IDENTITY,
+tabla1id2 INT NOT NULL,
+nombre nvarchar (40),
+
+CONSTRAINT pk_tabla1
+PRIMARY KEY (tabla1id,tabla1id2)
+);
+
+
+CREATE TABLE tabla2(
+idtabla2 int NOT NULL IDENTITY,
+nombre nvarchar(50),
+tabla1id int,
+tabla1id2 int,
+
+CONSTRAINT pk_tabla2
+PRIMARY KEY (idtabla2),
+
+CONSTRAINT fk_tabla2_tabla2
+FOREIGN KEY (tabla1id,tabla1id2)
+REFERENCES tabla1(
+);
+Go
+
+
+
+--crear tablas con cardinalida 1 a1 
+
+CREATE TABLE employe(
+ID INT NOT NULL IDENTITY,
+ap1 NVARCHAR(20) NOT NULL,
+ap2 NVARCHAR(20) not null,
+sexo CHAR (1) NOT NULL,
+salario MONEY NOT NULL
+CONSTRAINT pk_employe
+PRIMARY KEY (ID)
+
+);
+
+CREATE TABLE deparmet(
+id int NOT NULL IDENTITY,
+nombre NVARCHAR (30) NOT NULL,
+ubicacion NVARCHAR (30) NOT NULL,
+employeid int NOT NULL,
+
+CONSTRAINT pk_deparmet,
+PRIMARY KEY (id),
+CONSTRAINT fk
+
+);
